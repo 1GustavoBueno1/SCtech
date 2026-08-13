65ca3f798d8d8ffc1d65493b6183d73f06d2b978
gustavo <gustavo.silva123913@gmail.com>
2026-08-13 12:52:27 -0300

    calculadora com JS e Html

---
+++ aula19/calculadora/script.js
@@ -0,0 +1,27 @@
+function calcular() {
+
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operador = document.getElementById("operador").value;
+    let resultado;
+
+    switch (operador) {
+
+        case "+":
+            resultado = num1 + num2;
+            break;
+        case "-":
+            resultado = num1 - num2;
+            break
+        case "*":
+            resultado = num1 * num2;
+            break;
+        case "/":
+            resultado = num1 / num2;
+            break
+        default:
+            resultado = "Operador Invalido!"
+    }
+    document.getElementById("resultado").innerHTML = "Resultado:" + resultado;
+
+}
